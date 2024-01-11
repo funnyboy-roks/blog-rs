55fb59e821c5131fed6f58f4072e0becbd0b9c11
funnyboy-roks <funnyboyroks@gmail.com>
2024-01-11 16:37:09 -0600

    Add folders and proper maths support using a pulldown-cmark fork
    The fork: https://github.com/rhysd/pulldown-cmark/tree/math

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 use std::{
-    collections::{BTreeSet, HashMap},
+    collections::BTreeSet,
     fs,
     path::{Path, PathBuf},
     process::Command,
 };
 
 use anyhow::Context;
-use pulldown_cmark::{CowStr, Event, Options, Parser, Tag};
-use regex::{Regex, Replacer};
+use pulldown_cmark::{CowStr, Event, MathMode, Options, Parser, Tag};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use upon::Template;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+enum DirEntry {
+    File {
+        path: String,
+        frontmatter: Frontmatter,
+    },
+    Directory {
+        path: String,
+        info: Option<Frontmatter>,
+        contents: Vec<DirEntry>,
+    },
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, Default)]
 struct Frontmatter {
     title: String,
     description: String,
@@ -18,13 +31,271 @@ struct Frontmatter {
     date: chrono::NaiveDate,
 }
 
+#[derive(Clone, Debug, Serialize)]
+struct PageEntry {
+    file: String,
+    frontmatter: Option<Frontmatter>,
+    date_formatted: String,
+    is_dir: bool,
+}
+
+fn render_files(
+    template: &Template,
+    path: impl AsRef<Path>,
+    md_options: Options,
+    out_dir: impl AsRef<Path>,
+) -> anyhow::Result<DirEntry> {
+    let files = fs::read_dir(&path)?;
+    let out_path = path.as_ref().display().to_string();
+    let mut out_info: Option<Frontmatter> = None;
+    let mut last_updated = 0;
+    let mut out_contents = Vec::new();
+
+    for file in files {
+        let file = file?;
+        let metadata = file.metadata()?;
+        let ft = metadata.file_type();
+        let path = file.path();
+
+        let Some(file_name) = path.file_name() else {
+            continue;
+        };
+
+        let file_name = file_name.to_str().context("")?.to_string();
+
+        if ft.is_dir() {
+            let mut out_dir = out_dir.as_ref().to_path_buf();
+            out_dir.push(&file_name);
+
+            fs::create_dir_all(&out_dir)?;
+
+            out_contents.push(render_files(
+                &template,
+                format!("{}/{}", out_path, &file_name),
+                md_options,
+                out_dir,
+            )?);
+            continue;
+        }
+
+        if !ft.is_file() {
+            continue;
+        }
+
+        if file_name == "index.toml" {
+            out_info = Some(toml::from_str(&fs::read_to_string(path)?)?);
+            continue;
+        }
+
+        let Some(ext) = path.extension() else {
+            continue;
+        };
+
+        if ext != "md" {
+            continue;
+        }
+
+        last_updated = std::cmp::max(
+            last_updated,
+            metadata
+                .modified()?
+                .duration_since(std::time::UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
+        );
+
+        let name: String = file_name.trim_end_matches(".md").into();
+
+        eprintln!("rendering {}", path.display());
+
+        let content = fs::read_to_string(path)?;
+
+        let (frontmatter, content) = parse_frontmatter::<Frontmatter>(content)?;
+
+        let parser = Parser::new_ext(&content, md_options);
+
+        let mut heading_level = None;
+        let parser = parser.filter_map(|event| match event {
+            Event::Math(mm, s) => {
+                let opts = katex::Opts::builder()
+                    .display_mode(match mm {
+                        MathMode::Display => true,
+                        MathMode::Inline => false,
+                    })
+                    .output_type(katex::opts::OutputType::Mathml)
+                    .build()
+                    .unwrap();
+
+                let mut dst = String::new();
+                match katex::render_with_opts(&s, &opts) {
+                    Ok(ref ml) => dst.push_str(ml),
+                    Err(err) => {
+                        eprintln!("Maths error: {:?}\n{}", err, s);
+                        dst.push_str(&format!(
+                            r#"<span style="red">Maths Error: {:?}</span>"#,
+                            err
+                        ));
+                    }
+                }
+                Some(Event::Html(dst.into()))
+            }
+            Event::Start(Tag::Heading { level, .. }) => {
+                heading_level = Some(level);
+                None
+            }
+            Event::Text(text) => {
+                if let Some(heading_level) = heading_level.take() {
+                    let anchor = text
+                        .clone()
+                        .into_string()
+                        .trim()
+                        .to_lowercase()
+                        .replace(" ", "-");
+
+                    let out_tag = format!(
+                        r##"<{} id="{}"><a class="header" href="#{1}">{}</a>"##,
+                        heading_level, anchor, text
+                    );
+
+                    Some(Event::Html(CowStr::from(out_tag)))
+                } else {
+                    Some(Event::Text(text))
+                }
+            }
+            _ => Some(event),
+        });
+
+        let mut content = String::new();
+        pulldown_cmark::html::push_html(&mut content, parser);
+
+        let mut out_dir = out_dir.as_ref().to_path_buf();
+        out_dir.push(&name);
+
+        fs::create_dir_all(&out_dir)?;
+
+        out_dir.push("index.html");
+
+        let out = template
+            .render(upon::value! {
+                frontmatter: &frontmatter,
+                index: false,
+                rendered_body: &content,
+                head: None,
+            })
+            .to_string()
+            .with_context(|| format!("Error rendering {}", name))?;
+
+        let out = minify_html::minify(
+            out.as_bytes(),
+            &minify_html::Cfg {
+                do_not_minify_doctype: true,
+                ensure_spec_compliant_unquoted_attribute_values: true,
+                keep_closing_tags: true,
+                keep_html_and_head_opening_tags: true,
+                keep_spaces_between_attributes: false,
+                keep_comments: false,
+                minify_css: true,
+                minify_css_level_1: true,
+                minify_css_level_2: true,
+                minify_css_level_3: true,
+                minify_js: true,
+                remove_bangs: false,
+                remove_processing_instructions: false,
+            },
+        );
+
+        fs::write(out_dir, out)?;
+        out_contents.push(DirEntry::File {
+            path: format!("{}/{}", out_path, name.clone()),
+            frontmatter,
+        });
+    }
+
+    let (mut pages, dirs): (Vec<_>, Vec<_>) = out_contents
+        .iter()
+        .filter(|e| {
+            !PathBuf::from(match e {
+                DirEntry::File { path, .. } => path,
+                DirEntry::Directory { path, .. } => path,
+            })
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .starts_with('_')
+        })
+        .map(|e| match e {
+            DirEntry::File { path, frontmatter } => {
+                let path = PathBuf::from(path);
+                let path = PathBuf::from_iter(path.components().skip(1));
+                PageEntry {
+                    file: path.display().to_string(),
+                    frontmatter: Some(frontmatter.clone()),
+                    date_formatted: frontmatter.date.format("%d %B %Y").to_string(),
+                    is_dir: false,
+                }
+            }
+            DirEntry::Directory { path, info, .. } => {
+                let path = PathBuf::from(path);
+                let path = PathBuf::from_iter(path.components().skip(1));
+                PageEntry {
+                    file: path.display().to_string(),
+                    frontmatter: info.clone(),
+                    date_formatted: if let Some(fm) = info {
+                        fm.date.format("%d %B %Y").to_string()
+                    } else {
+                        "".into()
+                    },
+                    is_dir: true,
+                }
+            }
+        })
+        .partition(|e| !e.is_dir);
+
+    pages.sort_by_key(|p| {
+        (
+            std::cmp::Reverse(
+                p.frontmatter
+                    .clone()
+                    .expect("pages always have frontmatter")
+                    .date,
+            ),
+            p.file.clone(),
+        )
+    });
+
+    eprintln!("rendering Index for {}", out_path);
+    let out = template
+        .render(upon::value! {
+            title: out_info.clone().map(|fm| fm.title),
+            desc: out_info.clone().map(|fm| fm.description),
+            index: true,
+            pages: dirs.into_iter().chain(pages).collect::<Vec<_>>(),
+            head: None,
+        })
+        .to_string()
+        .context("Error rendering index")?;
+
+    let mut out_dir = out_dir.as_ref().to_path_buf();
+    out_dir.push("index.html");
+
+    fs::write(out_dir, out)?;
+
+    if let Some(ref mut out_info) = out_info {
+        out_info.date = chrono::NaiveDateTime::from_timestamp_opt(last_updated as i64, 0)
+            .unwrap()
+            .into();
+    }
+
+    Ok(DirEntry::Directory {
+        path: out_path,
+        info: out_info,
+        contents: out_contents,
+    })
+}
+
 fn main() -> anyhow::Result<()> {
     let time = std::time::Instant::now();
-
-    let files: Vec<_> = fs::read_dir("md")
-        .context("md dir not found")?
-        .filter_map(|d| d.ok())
-        .collect();
 
     let mut engine = upon::Engine::new();
 
@@ -83,204 +354,13 @@ fn main() -> anyhow::Result<()> {
     let md_options = Options::ENABLE_TABLES
         | Options::ENABLE_FOOTNOTES
         | Options::ENABLE_STRIKETHROUGH
-        | Options::ENABLE_TASKLISTS;
+        | Options::ENABLE_TASKLISTS
+        | Options::ENABLE_MATH;
 
-    let mut pages = HashMap::with_capacity(files.len());
-
-    for file in files {
-        let metadata = file.metadata()?;
-        let ft = metadata.file_type();
-        let path = file.path();
-
-        let Some(file_name) = path.file_name() else {
-            continue;
-        };
-
-        let file_name = file_name.to_str().context("")?.to_string();
-
-        if !ft.is_file() {
-            continue;
-        }
-
-        let Some(ext) = path.extension() else {
-            continue;
-        };
-
-        if ext != "md" {
-            continue;
-        }
-
-        let name: String = file_name.trim_end_matches(".md").into();
-
-        eprintln!("rendering {}", name);
-
-        // We know that `file` is now a md file
-
-        let content = fs::read_to_string(path)?;
-
-        let (frontmatter, content) = parse_frontmatter::<Frontmatter>(content)?;
-
-        let content = preprocess_content(&content);
-
-        let parser = Parser::new_ext(&content, md_options);
-
-        let mut heading_level = None;
-        let parser = parser.filter_map(|event| match event {
-            Event::Start(Tag::Heading(level, ..)) => {
-                heading_level = Some(level);
-                None
-            }
-            Event::Text(text) => {
-                if let Some(heading_level) = heading_level.take() {
-                    let anchor = text
-                        .clone()
-                        .into_string()
-                        .trim()
-                        .to_lowercase()
-                        .replace(" ", "-");
-                    let tmp = Event::Html(CowStr::from(format!(
-                        r##"<{heading_level} id="{anchor}"><a class="header" href="#{anchor}">{text}</a>"##,
-                    )))
-                    .into();
-                    return tmp;
-                } else {
-                    Some(Event::Text(text))
-                }
-            }
-            _ => Some(event),
-        });
-
-        let mut content = String::new();
-        pulldown_cmark::html::push_html(&mut content, parser);
-
-        let mut out_dir = out_dir.clone();
-        out_dir.push(&name);
-
-        fs::create_dir_all(&out_dir)?;
-
-        out_dir.push("index.html");
-
-        let out = layout_template
-            .render(upon::value! {
-                frontmatter: &frontmatter,
-                index: false,
-                rendered_body: &content,
-                head: None,
-            })
-            .to_string()
-            .with_context(|| format!("Error rendering {}", name))?;
-
-        let out = minify_html::minify(
-            out.as_bytes(),
-            &minify_html::Cfg {
-                do_not_minify_doctype: true,
-                ensure_spec_compliant_unquoted_attribute_values: true,
-                keep_closing_tags: true,
-                keep_html_and_head_opening_tags: true,
-                keep_spaces_between_attributes: false,
-                keep_comments: false,
-                minify_css: true,
-                minify_css_level_1: true,
-                minify_css_level_2: true,
-                minify_css_level_3: true,
-                minify_js: true,
-                remove_bangs: false,
-                remove_processing_instructions: false,
-            },
-        );
-
-        fs::write(out_dir, out)?;
-        pages.insert(name.clone(), frontmatter);
-    }
-
-    #[derive(Clone, Debug, Serialize)]
-    struct PageEntry {
-        file: String,
-        frontmatter: Frontmatter,
-        date_formatted: String,
-    }
-
-    let mut pages: Vec<_> = pages
-        .into_iter()
-        .filter(|(n, _)| !n.starts_with('_'))
-        .map(|(n, fm)| {
-            let date_formatted = fm.date.format("%d %B %Y").to_string();
-            PageEntry {
-                file: n.trim_end_matches(".md").into(),
-                frontmatter: fm,
-                date_formatted,
-            }
-        })
-        .collect();
-
-    pages.sort_by_key(|p| (std::cmp::Reverse(p.frontmatter.date), p.file.clone()));
-
-    eprintln!("rendering Index");
-    let out = layout_template
-        .render(upon::value! {
-            index: true,
-            pages: pages,
-            head: None,
-        })
-        .to_string()
-        .context("Error rendering index")?;
-
-    let mut out_dir = out_dir.clone();
-    out_dir.push("index.html");
-
-    fs::write(out_dir, out)?;
+    render_files(&layout_template, "md", md_options, out_dir)?;
 
     eprintln!("Completed rendering in {}ms", time.elapsed().as_millis());
     Ok(())
-}
-
-enum MathReplacer {
-    Block,
-    Inline,
-}
-
-impl Replacer for MathReplacer {
-    fn replace_append(&mut self, caps: &regex::Captures<'_>, dst: &mut String) {
-        let opts = katex::Opts::builder()
-            .display_mode(match self {
-                Self::Block => true,
-                Self::Inline => false,
-            })
-            .output_type(katex::opts::OutputType::Mathml)
-            .build()
-            .unwrap();
-
-        match katex::render_with_opts(&caps[1], &opts) {
-            Ok(ref ml) => {
-                dst.push_str(
-                    // Note: pulldown-cmark switches these back to the correct symbols
-                    &ml.replace("_", r"\_")
-                        .replace("*", r"\*")
-                        .replace("~", r"\~"),
-                );
-            }
-            Err(err) => {
-                dbg!(&caps[1]);
-                eprintln!("Maths error: {:?}", err);
-                dst.push_str(&format!(
-                    r#"<span style="red">Maths Error: {:?}</span>"#,
-                    err
-                ));
-            }
-        }
-    }
-}
-
-lazy_static::lazy_static! {
-    static ref MATH_BLOCK: Regex = Regex::new(r"(?s)\s\$\$(.+?)\$\$\s").unwrap();
-    static ref MATH_INLINE: Regex = Regex::new(r"(?s)[^\\$]\$(.+?)\$").unwrap();
-}
-
-fn preprocess_content(content: &str) -> String {
-    let out = MATH_BLOCK.replace_all(content, MathReplacer::Block);
-    let out = MATH_INLINE.replace_all(&out, MathReplacer::Inline);
-    let out = out.replace(r"\$", "$");
-    out.into()
 }
 
 fn parse_frontmatter<T>(content: String) -> anyhow::Result<(T, String)>
